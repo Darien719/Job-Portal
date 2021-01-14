1baa411f9d5f39027af98d965e65d7a8666cfc09
Darien719 <Dsosa029@fiu.edu>
2021-01-14 14:17:03 -0500

    Main Driver, spring orm dependency

---
+++ src/main/java/com/jobportal/MainDriver.java
@@ -0,0 +1,5 @@
+package com.jobportal;
+
+public class MainDriver {
+
+}
---
+++ src/main/java/com/jobportal/model/User.java
@@ -0,0 +1,139 @@
+package com.jobportal.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@Column(name="user_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int userId;
+	
+	@Column(name="first_name", nullable=false)
+	private String firstName;
+
+	@Column(name="last_name", nullable=false)
+	private String lastName;
+	
+	@Column(name="email", unique=true, nullable=false)
+	private String email;
+	
+	@Column(name="username", unique=true, nullable=false)
+	private String username;
+	
+	@Column(name="password", nullable=false)
+	private String password;
+	
+	@ManyToOne(cascade=CascadeType.ALL)
+	private int userRole;
+	
+	@ManyToOne(cascade=CascadeType.ALL)
+	private int companyId;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int userId, String firstName, String lastName, String email, String username, String password,
+			int userRole, int companyId) {
+		super();
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.userRole = userRole;
+		this.companyId = companyId;
+	}
+	
+	public User(String firstName, String lastName, String email, String username, String password,
+			int userRole, int companyId) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.userRole = userRole;
+		this.companyId = companyId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(int userRole) {
+		this.userRole = userRole;
+	}
+
+	public int getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(int companyId) {
+		this.companyId = companyId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", username=" + username + ", password=" + password + ", userRole=" + userRole + ", companyId="
+				+ companyId + "]";
+	}
+
+}
